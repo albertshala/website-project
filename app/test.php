149df8f48815c82eb94e7f6ccd88caa946238e99
Albert Shala <albert.shala@gmail.com>
2014-06-15 16:53:13 -0700

    test

--- app/test.php
+++ app/test.php
@@ -1,45 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: albert-shala
- * Date: 2014-06-14
- * Time: 10:49 PM
- */
-header('Content-Type: application/json');
-ini_set('display_errors', 1);
 require_once('TwitterAPIExchange.php');
-
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
     'oauth_access_token' => "71496187-DKfsbvxz4LV5t40pa1qnrL6lU7uJBqsExDybtzv7U",
     'oauth_access_token_secret' => "WcNxsyqv2pPncZRlrnCt5solInxkcqpBThNM7JtpjHgNm",
-    'consumer_key' => "HITQuRCHEVl3zrN9VRnnSY4Ut",
-    'consumer_secret' => "fmo4cDX1lXFBBbzcNQD8CU7jAdLLbToHDFyxoBtk6p9txd4tUH"
+    'consumer_key' => "KElZJH9UAa6dqnm2Kvi5T0mQB",
+    'consumer_secret' => "Q2Mvdpb9wYHy3EKy6BN7zt1u8WDF0OCtq39B0DKKxyVUUqojsW"
 );
-
-/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-$url = 'https://api.twitter.com/1.1/blocks/create.json';
-$requestMethod = 'POST';
-
-/** POST fields required by the URL above. See relevant docs as above **/
-$postfields = array(
-    'screen_name' => 'usernameToBlock',
-    'skip_status' => '1'
-);
-
-/** Perform a POST request and echo the response **/
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+if (isset($_GET['user'])) {$user = $_GET['user'];} else {$user = "iagdotme";}
+if (isset($_GET['count'])) {$user = $_GET['count'];} else {$count = 20;}
+$getfield = "?screen_name=$user&count=$count";
 $twitter = new TwitterAPIExchange($settings);
-echo $twitter->buildOauth($url, $requestMethod)
-    ->setPostfields($postfields)
-    ->performRequest();
-
-/** Perform a GET request and echo the response **/
-/** Note: Set the GET field BEFORE calling buildOauth(); **/
-$url = 'https://api.twitter.com/1.1/followers/ids.json';
-$getfield = '?screen_name=albert_shala';
-$requestMethod = 'GET';
-$twitter = new TwitterAPIExchange($settings);
-echo $twitter->setGetfield($getfield)
+$string = json_decode($twitter->setGetfield($getfield)
     ->buildOauth($url, $requestMethod)
-    ->performRequest();
-
+    ->performRequest(),$assoc = TRUE);
+if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+foreach($string as $items)
+{
+    echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+    echo "Tweet: ". $items['text']."<br />";
+    echo "Tweeted by: ". $items['user']['name']."<br />";
+    echo "Screen name: ". $items['user']['screen_name']."<br />";
+    echo "Followers: ". $items['user']['followers_count']."<br />";
+    echo "Friends: ". $items['user']['friends_count']."<br />";
+    echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+}
+?>
